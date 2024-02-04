8851c8aa2de02f661375423b6885a921826520d0
geen <u2421280501@163.com>
2024-02-05 00:38:33 +0800

    HJ17

--- letcode/src/com/algorithm/huawei/HJ17.java
+++ letcode/src/com/algorithm/huawei/HJ17.java
@@ -7,32 +7,44 @@ public class HJ17 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String code = null;
-        int x = 0;
-        int y = 0;
-        while (in.hasNextLine()) {
-            code = in.nextLine();
-        }
-       if (code==null) System.out.println("("+x+","+y+")");
-        List<String> operate = new ArrayList<>();
-        List<Integer> number = new ArrayList<>();
-        String[] split = code.split(";");
-        for (int i = 0; i <split.length ; i++) {
-            String s = split[i];
-            if(i%2==0){
 
+        while (in.hasNextLine()) {
+            String code = in.nextLine();
+            long x = 0;
+            long y = 0;
+            if (code==null) System.out.println(x+","+y);
+            String[] split = code.split(";");
+            for (int i = 0; i <split.length ; i++) {
+                String s = split[i];
+                if(s.length()<2||s.length()>4)continue;
+                char c = s.charAt(0);
+                if(!isLaw(c))continue;
+                String str = s.substring(1);
+                if(!isLaw(str))continue;
+                int num = Integer.parseInt(str);
+                if(c=='A')x-=num;
+                if(c=='S')y-=num;
+                if(c=='D')x+=num;
+                if(c=='W')y+=num;
             }
+            System.out.println(x+","+y);
         }
+
     }
 
-    public boolean isNumber(String s){
+    public static boolean isLaw(char c){
+        return c == 'A' || c == 'D' || c == 'W' || c == 'S';
+    }
+
+    public static boolean isLaw(String s){
         int number = 0;
         try {
-            number=Integer.valueOf(s);
-            return true;
+            number=Integer.parseInt(s);
+            return number < 100;
         }catch (Exception e){
             return false;
         }
     }
+
 
 }
